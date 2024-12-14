02c2cb4b025a5166d73d5e7935eaec9b70572150
ranseaa <xioranxrj@gmail.com>
2024-12-14 15:26:05 +0700

    first commit

---
+++ pp/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/de8de52639.js" crossorigin="anonymous"></script>
+    <title>ChooseNRead.com</title>
+    <link rel="icon" href="assets/logokecil.png" type="image/png">
+  </head>
+  <body>
+    <!--Navbar-->
+   <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary shadow-sm justify-space-between">
+  <div class="container-fluid">
+    <a href="sign/link_login.html" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#homeSection">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#aboutSection">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#footer">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <section id="homeSection" class="p-5">
+      <div class="d-flex flex-wrap justify-content-center">
+        <div class="col mt-5">
+         <h2 class="fw-bold text-success"><span class="text-primary">Choose</span>NRead</h2>
+          <p class="mb-4">"Jelajahi Dunia Pengetahuan Tanpa Batas: <br> Perpustakaan Digital <span class="fw-bold">ChooseNRead</span> untuk Petualangan Literasi Anda!"</p>
+          <a class="btn btn-primary" href="sign/link_login.html">Let Get Started</a>
+        </div>
+        <div class="col mt-3">
+          <img src="assets/logoDashboard-transformed.jpeg" width="450px">
+        </div>
+      </div>
+    </section>
+    
+    <section class="bg-body-secondary p-5" id="aboutSection">
+      <div class="row">
+          <div class="d-flex">
+            <h2 class="text-primary">About</h2>
+          </div>
+          <p>Dalam era digital ini, akses terhadap informasi sangatlah penting,
+             terutama dalam dunia pendidikan. Untuk mendukung pembelajaran yang lebih efektif dan efisien, 
+             kami menciptakan sebuah website perpustakaan digital yang inovatif. Website ini dirancang khusus 
+             untuk memudahkan Anda agar mengakses perpustakaan digital dimana saja dan kapan saja yang mudah dan cepat ke 
+             berbagai sumber daya pembelajaran atau mencari referensi buku bacaan. Dengan website perpustakaan kami, 
+             kami berharap dapat membantu Anda dan meningkatkan kualitas pendidikan Anda. Selamat belajar!</p>
+      </div>
+      <div class="d-flex justify-content-center">
+        <h3 class="text-secondary">Dikembangkan Oleh :</h3>
+      </div>
+
+    <div class="d-flex justify-content-center flex-wrap gap-4 mt-4">
+      <div class="card border-info mb-3" style="max-width: 18rem;">
+        <div class="card-header">Siti Ratna J.</div>
+        <div class="card-body">
+          <h5 class="card-title">Student At SMKN 1 Ciomas</h5>
+        </div>
+      </div>
+    </div>
+    </section>
+    
+    <footer id="footer" class="p-3 bg-dark">
+      <div class="row">
+        <div class="col">
+          <img src="assets/logofooter.png" width="200px">
+        </div>
+      </div>
+      <div class="row p-3">
+        <div class="col mt-3">
+          <h3 class="text-light fs-5">Alamat</h3>
+          <p class="text-secondary fs-6">JL Raya Laladon Ciomas - Bogor 16610</p>
+        </div>
+        <hr class="text-light mt-3">
+        <div class="d-flex justify-content-center gap-4">
+          <a href="https://api.whatsapp.com/send?phone=6289531311057" target="_blank" class="fs-3"><i class="fa-brands fa-whatsapp"></i></a>
+          <a href="https://www.tiktok.com/@smkn1ciomas" target="_blank" class="fs-3"><i class="fa-brands fa-tiktok"></i></a>
+          <a href="https://www.instagram.com/smkn1ciomas/" target="_blank" class="fs-3"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+        <div class="d-flex justify-content-center align-items-center mt-3">
+          <p class="text-light">Made with ❤️ <a href="https://api.whatsapp.com/send?phone=6289531311057" target="_blank" class="">Siti Ratna J.</a>  2024</p>
+        </div>
+      </div>
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
